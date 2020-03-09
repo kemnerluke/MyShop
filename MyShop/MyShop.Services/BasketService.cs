899a2050821a1c56cb1e7c9c19a9fa71b113652e
Luke <kemnerluke@gmail.com>
2020-03-09 12:13:30 -0400

    created basket UI

--- MyShop/MyShop.Services/BasketService.cs
+++ MyShop/MyShop.Services/BasketService.cs
@@ -94,15 +94,15 @@ namespace MyShop.Services
             basketContext.Commit();
         }
 
-        public void RemoveFromBasket(HttpContextBase httpContext, string productId)
+        public void RemoveFromBasket(HttpContextBase httpContext, string itemId)
         {
             Basket basket = GetBasket(httpContext, true);
-            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.ProductId == productId);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.Id == itemId);
 
             if (item != null)
             {
                 basket.BasketItems.Remove(item);
-                basket.BasketItems.Add(item);
+                basketContext.Commit();
             }
         }
 
